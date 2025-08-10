3b0238e8923bc8549d27ce039700b014789ae95c
masaki1002 <masakioeda1002@yahoo.com>
2025-08-10 10:27:31 +0900

    added yeah

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Route} from 'react-router-dom';
 function App() {
   return (
     <div>
-      <p>Hinata homepage</p>
+      <p>Hinata homepage, yeah</p>
     </div>
   );
 }
