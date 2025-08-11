a3f2872db946a5072ace0712f905bb3cae2a102e
masaki1002 <masakioeda1002@yahoo.com>
2025-08-11 20:59:00 +0900

    bringing back hashrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react"
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home";
 import MorePosts from "./components/MorePosts";
 
 function App() {
   return (
-    <Router>
+       <Router>
        <Routes>
         <Route path="/" element={ <Home /> }></Route>
         <Route path="/#" element={ <Home /> }></Route>
         <Route path="/moreposts" element={ <MorePosts/> }></Route>
        </Routes>
     </Router>
+    
   );
 }
 
