2040724fb7306119a7efa58cbd4677b2064082a4
masaki1002 <masakioeda1002@yahoo.com>
2025-08-13 09:59:54 +0900

    the entire homepage in draft

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./components/Navigation";
 import Top from "./components/Top";
 import Post from "./components/Post";
+import Intro from "./components/Intro";
+import Approach from "./components/Approach";
+import PatientVoices from "./components/PatientVoices";
+import Members from "./components/Members";
+import Menus from "./components/Menus";
+import AccessMap from "./components/AccessMap";
+import SNS from "./components/SNS";
 
 function App() {
   return (
@@ -12,6 +19,13 @@ function App() {
       <Navigation />
       <Top />
       <Post />
+      <Intro />
+      <Approach />
+      <PatientVoices />
+      <Members />
+      <Menus />
+      <AccessMap />
+      <SNS /> 
     </div>
     
   );
