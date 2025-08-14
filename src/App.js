c1042fbd6d941ef4289bfc23c1f854a25babb15f
masaki1002 <masakioeda1002@yahoo.com>
2025-08-14 10:13:52 +0900

    using hashrouter for now

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import React from "react"
 
 import './App.css';
+import { Routes, Route, HashRouter  } from 'react-router-dom';
+import Post from './components/Post';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./components/Navigation";
-import Top from "./components/Top";
-import Post from "./components/Post";
-import Intro from "./components/Intro";
-import Approach from "./components/Approach";
-import PatientVoices from "./components/PatientVoices";
-import Members from "./components/Members";
-import Menus from "./components/Menus";
-import AccessMap from "./components/AccessMap";
-import SNS from "./components/SNS";
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import MorePosts from "./components/MorePosts";
 import Home from "./components/Home";
+import MorePosts from "./components/MorePosts";
 
 function App() {
   return (
-
-      <div className="all_components">
-
-        <Router>
+    <HashRouter>
     
        <Routes>
         <Route path="/" element={ <Home /> }></Route>
+        <Route path="/#" element={ <Home /> }></Route>
         <Route path="/moreposts" element={ <MorePosts/> }></Route>
        </Routes>
-    </Router>
-
-    </div>
-    
+    </HashRouter>
   );
 }
 
 export default App;
-
