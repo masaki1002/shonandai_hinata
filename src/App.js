458500e2d25361374bee18d3311ab23aed0dfd4e
masaki1002 <masakioeda1002@yahoo.com>
2025-08-14 09:49:54 +0900

    tweaked the path

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
     
        <Routes>
         <Route path="/" element={ <Home /> }></Route>
-        <Route path="/#" element={ <Home /> }></Route>
         <Route path="/moreposts" element={ <MorePosts/> }></Route>
        </Routes>
     </Router>
